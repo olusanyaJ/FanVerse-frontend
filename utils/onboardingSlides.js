a0c1a639fc47a8555591d352f4863394f6184d09
Olusanya Jegede <olusanyajegede1@gmail.com>
2024-02-26 10:23:16 -0600

    logo fix

--- utils/onboardingSlides.js
+++ utils/onboardingSlides.js
@@ -4,20 +4,20 @@ export default [
     title: "Your hub for live action 🔥",
     description:
       "Catch every thrilling moment with real-time updates, live scores, and play-by-play commentary. Don't miss a beat!",
-    image: require("../assets/one.png"),
+    image: require("../assets/frameOne.png"),
   },
   {
     id: "2",
     title: "Your info personalised for you 🚨",
     description:
       "Keep track of your favourite teams and leagues. Never miss out on the trending news ands stories.",
-    image: require("../assets/one.png"),
+    image: require("../assets/frameTwo.png"),
   },
   {
     id: "3",
     title: "Your sports tribe awaits 🚀",
     description:
       "Build connections that go beyond the app. Find your sports family. Join discussions, share insights, and stay updated on all the latest buzz.",
-    image: require("../assets/one.png"),
+    image: require("../assets/frameThree.png"),
   },
 ];
